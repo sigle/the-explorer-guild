902d19cf4516eb1b620b63137a260eb7ea3016de
Pradel Léo <pradel.leo@gmail.com>
2021-10-31 14:55:22 +0100

    fix comment

--- contracts/tests/config.ts
+++ contracts/tests/config.ts
@@ -1,4 +1,4 @@
 // Number of items in the collection
 export const maxMintNumber = 10000;
-// Number of items in the collection
+// Number of items that can be minted for promotion
 export const promoMintNumber = 150;
