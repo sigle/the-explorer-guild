902b652ad0aebc8504ed5730b7298e2231c50a7c
Pradel Léo <pradel.leo@gmail.com>
2021-11-01 10:08:04 +0100

    more tests

--- contracts/tests/the-explorer-guild/set-provenance-hash.test.ts
+++ contracts/tests/the-explorer-guild/set-provenance-hash.test.ts
@@ -18,7 +18,7 @@ Clarinet.test({
 });
 
 Clarinet.test({
-  name: "[set-provenance-hash] Contract owner can update mint price properly",
+  name: "[set-provenance-hash] Contract owner can set provenance hash properly",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     let wallet_deployer = accounts.get("deployer")!;
     let block = chain.mineBlock([
@@ -45,5 +45,29 @@ Clarinet.test({
     block.receipts[0].result.expectOk().expectAscii("");
     block.receipts[1].result.expectOk().expectBool(true);
     block.receipts[2].result.expectOk().expectAscii("test");
+  },
+});
+
+Clarinet.test({
+  name: "[set-provenance-hash] Contract owner can set provenance hash only once",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let wallet_deployer = accounts.get("deployer")!;
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "the-explorer-guild",
+        "set-provenance-hash",
+        [types.ascii("test")],
+        wallet_deployer.address
+      ),
+      Tx.contractCall(
+        "the-explorer-guild",
+        "set-provenance-hash",
+        [types.ascii("test")],
+        wallet_deployer.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectOk().expectBool(true);
+    block.receipts[1].result.expectErr().expectUint(16);
   },
 });
