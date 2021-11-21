ae328eaca230acf4cd31d4d8d573b19912d34973
Pradel Léo <pradel.leo@gmail.com>
2021-11-21 18:14:37 +0100

    mint 71 in one tx

--- contracts/contracts/the-explorer-guild-museum.clar
+++ contracts/contracts/the-explorer-guild-museum.clar
@@ -2,18 +2,23 @@
 (define-constant err-failed-to-transfer-stx (err u10))
 (define-constant err-failed-to-transfer-nft (err u11))
 
-;; Mint 497 Explorers
-(define-public (mint-497)
+;; Amount of mint allowed for the team, giveways, promotion etc..
+(define-data-var promo-mint-number uint u150)
+
+;; TODO throw error if trying to mint more than 497 via this function
+
+;; Mint up to 497 Explorers in total
+;; Calling this function will mint 71 NFTs. 71 * 7 = 497
+(define-public (mint)
   (begin
     ;; Enable the sale so contract can mint
     (try! (contract-call? .the-explorer-guild-mint enable-sale))
-    ;; Transfer 497 STX to the contract in order to mint
-    (unwrap! (stx-transfer? u497 contract-owner (as-contract tx-sender)) err-failed-to-transfer-stx)
+    ;; Transfer 71 STX to the contract in order to mint
+    (unwrap! (stx-transfer? u71 contract-owner (as-contract tx-sender)) err-failed-to-transfer-stx)
     ;; Reduce contract price in order to mint
-    ;; Not possible to set to 0 as stx-transfer will fail
+    ;; Not possible to set to 0 as stx-transfer will fail with u3
     (try! (contract-call? .the-explorer-guild set-mint-price u1))
-    ;; TODO mint 497, 71 * 7
-    (try! (claim-transfer))
+    (map claim-transfer (list u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24 u25 u26 u27 u28 u29 u30 u31 u32 u33 u34 u35 u36 u37 u38 u39 u40 u41 u42 u43 u44 u45 u46 u47 u48 u49 u50 u51 u52 u53 u54 u55 u56 u57 u58 u59 u60 u61 u62 u63 u64 u65 u66 u67 u68 u69 u70 u71))
     ;; Restore mint price
     (try! (contract-call? .the-explorer-guild set-mint-price u100000000))
     ;; Disable the sale to not allow new mints
@@ -21,7 +26,7 @@
     (ok true)))
 
 ;; Claim an NFT and transfer it to the contract owner wallet
-(define-private (claim-transfer)
+(define-private (claim-transfer (index uint))
   (let (
     (claim-id (try! (as-contract (contract-call? .the-explorer-guild-mint claim))))
   )
--- contracts/tests/the-explorer-guild-museum/mint-497.test.ts
+++ contracts/tests/the-explorer-guild-museum/mint-497.test.ts
@@ -1,13 +1,13 @@
 import { Clarinet, Tx, Chain, Account, assertEquals } from "../packages.ts";
 
 Clarinet.test({
-  name: "[mint-497] Should throw error if non contract owner try to mint",
+  name: "[mint] Should throw error if non contract owner try to mint",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     let wallet_1 = accounts.get("wallet_1")!;
     let block = chain.mineBlock([
       Tx.contractCall(
         "the-explorer-guild-museum",
-        "mint-497",
+        "mint",
         [],
         wallet_1.address
       ),
@@ -18,18 +18,15 @@ Clarinet.test({
 });
 
 Clarinet.test({
-  name: "[mint-497] Should mint 497 to the deployer wallet",
+  name: "[mint] Should mint 71 NFTs to the deployer wallet",
   async fn(chain: Chain, accounts: Map<string, Account>) {
-    // TODO change to 497
-    let numberToMint = 7;
+    let numberToMint = 71;
     let wallet_deployer = accounts.get("deployer")!;
-
-    // TODO mint 2503 before
 
     let block = chain.mineBlock([
       Tx.contractCall(
         "the-explorer-guild-museum",
-        "mint-497",
+        "mint",
         [],
         wallet_deployer.address
       ),
@@ -37,7 +34,8 @@ Clarinet.test({
 
     block.receipts[0].result.expectOk().expectBool(true);
 
-    const events = block.receipts[0].events;
+    let events = block.receipts[0].events;
+    let assetMaps = chain.getAssetsMaps();
 
     // 1. Top up contract balance
     events[0].stx_transfer_event.sender.expectPrincipal(
@@ -46,12 +44,13 @@ Clarinet.test({
     events[0].stx_transfer_event.recipient.expectPrincipal(
       `${wallet_deployer.address}.the-explorer-guild-museum`
     );
-    events[0].stx_transfer_event.amount.expectInt(497);
+    events[0].stx_transfer_event.amount.expectInt(71);
 
-    // 2. Mint 497 NFTs in the museum contract
+    // 2. Mint 71 NFTs in the museum contract
     for (let index = 0; index < numberToMint; index++) {
-      const baseIndex = index * 2;
+      const baseIndex = index * 3;
 
+      // 2.1. Check that 1 STX was sent
       let { sender, recipient, amount } =
         events[baseIndex + 1].stx_transfer_event;
       sender.expectPrincipal(
@@ -60,6 +59,7 @@ Clarinet.test({
       recipient.expectPrincipal(wallet_deployer.address);
       amount.expectInt(1);
 
+      // 2.2. Check that 1 NFT was minted into the museum contract wallet
       let {
         asset_identifier,
         recipient: recipientMintEvent,
@@ -72,14 +72,10 @@ Clarinet.test({
         `${wallet_deployer.address}.the-explorer-guild-museum`
       );
       value.expectUint(index + 1);
-    }
 
-    // 3. Send all the NFTs to the owner wallet
-    for (let index = 0; index < numberToMint; index++) {
-      const baseIndex = index + numberToMint * 2;
-
-      let { asset_identifier, sender, recipient, value } =
-        events[baseIndex + 1].nft_transfer_event;
+      // 2.3. Check that the NFT was transferred from the museum contract wallet to the owner wallet
+      ({ asset_identifier, sender, recipient, value } =
+        events[baseIndex + 3].nft_transfer_event);
       asset_identifier.expectPrincipal(
         `${wallet_deployer.address}.the-explorer-guild::The-Explorer-Guild`
       );
@@ -89,8 +85,6 @@ Clarinet.test({
       recipient.expectPrincipal(wallet_deployer.address);
       value.expectUint(index + 1);
     }
-
-    let assetMaps = chain.getAssetsMaps();
 
     assertEquals(
       assetMaps.assets[".the-explorer-guild.The-Explorer-Guild"][
@@ -102,7 +96,7 @@ Clarinet.test({
       assetMaps.assets[".the-explorer-guild.The-Explorer-Guild"][
         wallet_deployer.address
       ],
-      7
+      71
     );
   },
 });
