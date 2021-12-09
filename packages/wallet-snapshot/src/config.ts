e261688a4e3d6dbffb8471bcac5d982687fde891
Léo Pradel <pradel.leo@gmail.com>
2021-12-09 19:28:50 +0100

    feat: wallet-snapshot collection size config (#18)

--- packages/wallet-snapshot/src/config.ts
+++ packages/wallet-snapshot/src/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   contractAddress: "SP2X0TZ59D5SZ8ACQ6YMCHHNR2ZN51Z32E2CJ173",
   contractName: "the-explorer-guild",
+  collectionSize: 2503,
   stacksArtCollectionId: 16,
 };
