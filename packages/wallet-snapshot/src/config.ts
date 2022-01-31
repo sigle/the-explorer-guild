929fa9e5d586b8e959446a9a6d7a8835fb994f6e
Léo Pradel <pradel.leo@gmail.com>
2022-01-31 17:32:31 +0100

    fix: update collection size in scripts (#21)

--- packages/wallet-snapshot/src/config.ts
+++ packages/wallet-snapshot/src/config.ts
@@ -3,7 +3,7 @@ import { StacksMainnet } from "@stacks/network";
 export const config = {
   contractAddress: "SP2X0TZ59D5SZ8ACQ6YMCHHNR2ZN51Z32E2CJ173",
   contractName: "the-explorer-guild",
-  collectionSize: 2503,
+  collectionSize: 3000,
   stacksArtCollectionId: 16,
   network: new StacksMainnet(),
   // You can use your own node easily by changing the following line
