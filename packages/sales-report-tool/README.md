1134b51c31b06fedc810eb70fc9514ba1ea28d36
Arturs Kirtovskis <arturs.kirtovskis@gmail.com>
2022-03-04 17:50:34 +0200

    feat: sales report package (#22)
    Co-authored-by: Léo Pradel <pradel.leo@gmail.com>

---
+++ packages/sales-report-tool/README.md
@@ -0,0 +1,22 @@
+# sales-report-tool
+
+This is a small package that can be used to export
+your NFT project sales data to CSV file.
+
+## How to use the tool?
+
+1. Go to `index.js` line 17 and change address to your minting contract address
+2. Go to `index.js` line 19 and change mint price to your project mint price
+3. Go to `index.js` line 60 to check whether you need to add any custom logic for folks wwho minted from CLI
+4. Run `pnpm install` and `pnpm run`
+5. After completing these steps you will gave `data.csv` in your folder
+
+## What rows does the CSV contain
+
+- Number of the purchase
+- Time
+- Transaction ID
+- Address of the buyer
+- Amount of STX spent
+- Amount of NFTs bought
+- Total amount of NFTs sold up to this point
---
+++ packages/sales-report-tool/index.ts
@@ -0,0 +1,96 @@
+import {
+  getFormattedDate,
+  getAmountOfNFTs,
+  TransactionFromAPI,
+  ResponseFromTransactionRequest,
+} from "./utils";
+import { writeFileSync } from "fs";
+import { fetch } from "undici";
+
+interface Transaction {
+  number: number;
+  time: string;
+  txnId: string;
+  address: string;
+  amountOfSTX: number;
+  amountOfNFTsBought: number;
+  totalAmountOfNFTsSold: number;
+}
+
+// Array for storing transactions
+const arrOfTxn: Transaction[] = [];
+let total: number;
+// Minting wallet address
+const address = `SP2X0TZ59D5SZ8ACQ6YMCHHNR2ZN51Z32E2CJ173`;
+// Mint price
+const mintPrice = 100;
+
+const getAccounting = async () => {
+  await fetch(
+    `https://stacks-node-api.mainnet.stacks.co/extended/v1/address/${address}/transactions`
+  )
+    .then((response: any) => response.json())
+    .then((data: ResponseFromTransactionRequest) => {
+      total = data.total;
+    });
+  let indexOfLastTxn = total;
+  let index = 1;
+  let nftsSold = 0;
+
+  while (indexOfLastTxn > 0 && indexOfLastTxn <= total) {
+    await fetch(
+      `https://stacks-node-api.mainnet.stacks.co/extended/v1/address/${address}/transactions?offset=${
+        indexOfLastTxn - 20
+      }`
+    )
+      .then((response: any) => response.json())
+      .then((data: ResponseFromTransactionRequest) => {
+        data?.results?.forEach((txn: any) => {
+          if (
+            txn.sender_address !== address &&
+            txn.tx_type === "contract_call"
+          ) {
+            nftsSold =
+              nftsSold + getAmountOfNFTs(txn.contract_call.function_name);
+            arrOfTxn.push({
+              number: index,
+              time: getFormattedDate(txn.burn_block_time),
+              txnId: txn.tx_id,
+              address: txn.sender_address,
+              amountOfSTX:
+                getAmountOfNFTs(txn.contract_call.function_name) * mintPrice,
+              amountOfNFTsBought: getAmountOfNFTs(
+                txn.contract_call.function_name
+              ),
+              totalAmountOfNFTsSold: nftsSold,
+            });
+            index = index + 1;
+          }
+        });
+      });
+    if (indexOfLastTxn < 20) {
+      indexOfLastTxn = 20;
+    } else {
+      indexOfLastTxn = indexOfLastTxn - 20;
+    }
+  }
+  if (arrOfTxn.length > 0) {
+    console.log("About to insert ", arrOfTxn.length);
+    writeFileSync(
+      "./data.csv",
+      `number,time,txnId, address, amountOfSTX, amountOfNFTsBought, totalAmountOfNFTsSold
+    ${arrOfTxn
+      .map(
+        (txn) =>
+          `${txn.number},${txn.time},${txn.txnId},${txn.address},${txn.amountOfSTX},${txn.amountOfNFTsBought},${txn.totalAmountOfNFTsSold}`
+      )
+      .join("\n")}
+    `,
+      { encoding: "utf8" }
+    );
+
+    console.log("Created file data.csv");
+  }
+};
+
+getAccounting();
